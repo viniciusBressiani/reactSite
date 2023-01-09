3d430c5827207b5fd3d1f373af5a2b83b6fa5fc6
viniciusBressiani <88894499+viniciusBressiani@users.noreply.github.com>
2023-01-09 11:11:37 -0300

    Finished Descrição

--- site-escm/src/components/Descricao.jsx
+++ site-escm/src/components/Descricao.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material';
+import copos from 'assets/Copos@x2W.webp';
+
+const description = [
+ "Beber cerveja é bom, mas poder beber uma cerveja produzida na sua própria casa é fantástico!",
+
+  "Com o curso de Cervejeiro Caseiro da ESCM você irá mergulhar no universo das cervejas e conhecer ferramentas para produzir a sua própria bebida, em casa, dentro dos padrões técnicos e de qualidade.",
+  
+  "Além disso, você conhecerá todo o processo de produção de cerveja, será capaz de fazer melhores escolhas de insumos e equipamentos e elaborará a sua própria receita de cerveja! Pelas câmeras, você acompanhará toda a preparação prática da cerveja que acontece em nossos laboratórios e discutirá, ao vivo com os professores e colegas da turma, os métodos e os fundamentos da ciência por trás da cerveja.",
+  
+  "Público-Alvo",
+  "O curso de Cervejeiro Caseiro foi desenhado para:",
+  "– você que nunca fez cerveja antes e quer aprender os fundamentos da arte com professores cervejeiros experientes;",
+  "– você que já faz cerveja em casa, mas quer entender onde tem errado para melhorar a qualidade da sua cerveja.",
+]
 
 const Descricao = () => {
   return (
-    <div>
-      Descrição
-    </div>
+    <Box>
+      <Box
+        sx={{
+          display:"flex",
+          marginTop: "8rem"
+        }}
+      >
+        <Box sx={{flex:1, background: "green", height: "5px"}}></Box>
+        <Box sx={{flex:1, background: "red", height: "5px"}}></Box>
+        <Box sx={{flex:1, background: "yellow", height: "5px"}}></Box>
+      </Box>
+      <Box
+        sx={{
+          marginTop: "8rem",
+          display: "grid",
+          gridTemplateColumns: "calc(47% - 1.5rem) 1fr",
+          gap:"3rem",
+          maxWidth: "1140px",
+          marginInline: "auto"
+        }}
+      >
+        <Box 
+        sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center"
+        }}>
+          <img width="85%"  src={copos} alt="Copos de Cerveja" />
+        </Box>
+        <Box>
+          <Typography variant='h2'
+          sx={{
+            margin: "4rem auto",
+          }}>No curso de Cervejeiro Caseiro você irá:</Typography>
+          {
+            description.map((text) => {
+              return <Box marginBottom="2rem">
+                <Typography variant='body1'>{text}</Typography>
+              </Box>
+            })
+          }
+      
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
--- site-escm/src/components/Professores.jsx
+++ site-escm/src/components/Professores.jsx
@@ -5,29 +5,41 @@ import Professor from './Professor';
 
 const Professores = () => {
   return (
-    <Box
-      sx={{
-        
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        maxWidth:"calc(1140px + 6rem)",
-        marginInline: "auto"
-      }}>
-        <Typography variant='h2' margin="6rem auto">Nossos Professores são altamente capacitados:</Typography>
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 1fr)",
-            alignContent: "center",
-            gap:"6rem"
-          }}>
-      {
-        professores.CCO.map(({title, description, image}, index) => {
-          return <Professor key={index} title={title} description={description} image={image} />
-        })
-      }
-    </Box>
+    <Box>
+       <Box
+        sx={{
+          display:"flex",
+          marginTop: "8rem"
+        }}
+      >
+        <Box sx={{flex:1, background: "green", height: "5px"}}></Box>
+        <Box sx={{flex:1, background: "red", height: "5px"}}></Box>
+        <Box sx={{flex:1, background: "yellow", height: "5px"}}></Box>
+      </Box>
+      <Box
+        sx={{
+      
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          maxWidth:"calc(1140px + 6rem)",
+          marginInline: "auto"
+        }}>
+          <Typography variant='h2' margin="6rem auto">Nossos Professores são altamente capacitados:</Typography>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              alignContent: "center",
+              gap:"6rem"
+            }}>
+        {
+          professores.CCO.map(({title, description, image}, index) => {
+            return <Professor key={index} title={title} description={description} image={image} />
+          })
+        }
+      </Box>
+      </Box>
     </Box>
     
   )
